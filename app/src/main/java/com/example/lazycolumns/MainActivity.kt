ef2ab87532d5277c924ba32009e537fb7947ab85
tumusiimeatuhaire-jpg <tumusiimeatuhaire@gmail.com>
2026-04-21 17:19:20 +0300

    A few more data

--- app/src/main/java/com/example/lazycolumns/MainActivity.kt
+++ app/src/main/java/com/example/lazycolumns/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lazycolumns.ui.theme.LazyColumnsTheme
 import ug.ac.ndejje.ronald.Student
@@ -99,5 +100,13 @@ fun StudentRow(student: Student) {
                 }
             }
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StudentListPreview() {
+    LazyColumnsTheme {
+        StudentListScreen()
     }
 }
